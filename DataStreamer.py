33acc29432669eb9e1269db99dd8e56756e25afd
Sven Sager <akira@narux.de>
2022-05-04 22:13:34 +0200

    Using own cycle because of problem with eventlet module and internal thread

--- DataStreamer.py
+++ DataStreamer.py
@@ -17,12 +17,13 @@ class DataStreamer:
             def __init__(self, socketio):
                 self._socketio = socketio
                 print("reaches here...")
-                self._revpi = revpimodio2.RevPiModIO(autorefresh=True, debug=True) # this line hangs
+                self._revpi = revpimodio2.RevPiModIO(debug=True) # this line hangs with autorefresh=True
                 print("never reaches here when above line is not commented out...")
 
             def _cycle_program(self, ct):
-                new_data = randint(-500, 500)/100
-                # new_data = self._revpi.io.InputValue_1.value/1000
+                self._revpi.readprocimg()  # Read all input values from process image
+                # new_data = randint(-500, 500)/100
+                new_data = self._revpi.io.InputValue_1.value / 1000
                 self._socketio.emit("new_data", {"data" : new_data})
 
             def produce(self):
